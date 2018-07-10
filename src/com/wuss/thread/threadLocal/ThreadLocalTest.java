80a0bf767324f861db539c0c0479011bd5839b88
wuss <wuss@wjs.com>
2018-07-10 14:26:58 +0800

    ThreadLocal 例子

--- src/com/wuss/thread/threadLocal/ThreadLocalTest.java
+++ src/com/wuss/thread/threadLocal/ThreadLocalTest.java
@@ -2,7 +2,11 @@ package com.wuss.thread.threadLocal;
 
 public class ThreadLocalTest {
     public static void main(String[] args) {
+        //主线程 设置 名称
         Account account = new Account("初始名");
+        for(int i=0;i<10;i++){
+            System.out.println(account.getName()+"\ti = " + i);
+        }
         new Test(account,"threadA：").start();
         new Test(account,"threadB：").start();
     }
